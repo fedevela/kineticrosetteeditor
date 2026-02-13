a1bd17276bece06101f68dcaedebb173fd30afcc
Federico Vela <fedevela@gmail.com>
2026-02-13 13:47:57 -0500

    initial app is now stable

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white text-black dark:bg-black dark:text-white">
+      <h1 className="text-5xl font-bold">Hello World</h1>
+    </main>
+  );
+}
